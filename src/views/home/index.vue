dcb8af4d66524088c80104ebae67598b5d8f870c
unknown <you@example.com>
2020-03-18 23:04:04 +0800

    主页布局-下拉加载-上拉刷新

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,90 @@
 <template>
-  <div class='container'>
-      我是主页
+  <div class="container">
+      <!-- 放置tabs组件 -->
+      <van-tabs>
+         <!-- 内部需要放置子 标签  title值为当前显示的内容-->
+         <!-- van-tab是vant组件的样式  -->
+         <van-tab :title="`标签${item}`" v-for="item in 10" :key="item">
+           <!-- 单元格 -->
+           <!-- <van-cell-group>
+                <van-cell title="标题" value="内容" :key="item" v-for="item in 20"></van-cell>
+              </van-cell-group> -->
+
+             <ArticleList></ArticleList>
+         </van-tab>
+      </van-tabs>
+      <!-- 在tabs下放置图标  编辑频道的图标 -->
+      <span class="bar_btn">
+        <!-- 放入图标 vant图标 -->
+         <van-icon name='wap-nav'></van-icon>
+      </span>
   </div>
 </template>
 
 <script>
+import ArticleList from '@/views/home/components/article-list'
 export default {
-  name: 'home' // devtools查看组件时  可以看到 对应的name名称
+  name: 'home', // devtools查看组件时  可以看到 对应的name名称
+  components: {
+    ArticleList
+  }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content{
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane{
+    height: 100%;
+    .scroll-wrapper{
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>

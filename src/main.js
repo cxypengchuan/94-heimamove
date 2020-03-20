1faffeb9d24ee46a0aee89297284683eab965f8b
unknown <you@example.com>
2020-03-20 20:44:04 +0800

    时间格式处理

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import Vant from 'vant' // 引入vant组件库
 import 'vant/lib/index.less' // 引入vant组件的样式
 import 'amfe-flexible'
 import '@/permission'// 引入路由导航守卫
+import plugin from '@/utils/plugin' // 引入插件
 import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
 Vue.use(Vant) // 注册Vant的所有组件 一旦注册任意位置都可以使用vant的组件 相当于调用了vant的install方法吧
 // 以上做法是完整导入的模式
+Vue.use(plugin) // 注册组件  需要放置在Vue.use(vant) 之后 因为我们要在 plugin中用vant的内置函数,必须等到vant注册完毕
 Vue.config.productionTip = false
 
 new Vue({

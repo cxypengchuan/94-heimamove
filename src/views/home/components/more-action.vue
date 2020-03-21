54478ef30f106bcd0af5f70b7d56c5372114f01d
unknown <you@example.com>
2020-03-21 18:45:31 +0800

    反馈垃圾内容-真实数据

--- src/views/home/components/more-action.vue
+++ src/views/home/components/more-action.vue
@@ -8,22 +8,25 @@
     </van-cell-group>
     <van-cell-group v-else>
       <van-cell icon="arrow-left" @click="isReport=false">返回</van-cell>
-      <van-cell>侵权</van-cell>
-      <van-cell>色情</van-cell>
+      <van-cell v-for="item in reports" :key="item.value">{{item.label}}</van-cell>
+      <!-- <van-cell>色情</van-cell>
       <van-cell>暴力</van-cell>
       <van-cell>低俗</van-cell>
       <van-cell>不适</van-cell>
       <van-cell>错误</van-cell>
-      <van-cell>其他</van-cell>
+      <van-cell>其他</van-cell> -->
     </van-cell-group>
   </div>
 </template>
 
 <script>
+import { reports } from '@/api/constants'
 export default {
   data () {
     return {
-      isReport: false
+      isReport: false,
+      reports
+
     }
   }
 }

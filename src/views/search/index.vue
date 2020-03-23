ca46de130676e80b2a9adfa1496b354e0c34de00
unknown <you@example.com>
2020-03-23 20:38:24 +0800

    删除历史记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -4,9 +4,9 @@
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
     <!-- 搜索组件 -->
-    <van-search  v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
+    <van-search v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
     <!-- 联想内容  有输入内容时 显示联想 -->
-    <van-cell-group class="suggest-box" v-if="q" >
+    <van-cell-group class="suggest-box" v-if="q">
       <van-cell icon="search">
         <span>j</span>ava
       </van-cell>
@@ -14,14 +14,19 @@
     <!-- 历史记录部分  你搜索的内容 会在这里记录 -->
     <!-- <div class="history-box" v-if="!q"> -->
     <div class="history-box" v-else>
-      <div class="head">
+      <div class="head" v-if="historyList.length">
         <span>历史记录</span>
         <van-icon name="delete"></van-icon>
       </div>
       <van-cell-group>
-        <van-cell>
-          <a class="word_btn">电脑</a>
-          <van-icon class="close_btn" slot="right-icon" name="cross" />
+        <van-cell @click="toResult(item)" v-for="(item,index) in historyList" :key="index">
+          <a class="word_btn">{{item}}</a>
+          <van-icon
+            @click.stop="delHistory(index)"
+            class="close_btn"
+            slot="right-icon"
+            name="cross"
+          />
         </van-cell>
       </van-cell-group>
     </div>
@@ -29,11 +34,25 @@
 </template>
 
 <script>
+const key = 'hm-94-toutiao-history' // 此key用来作为 历史记录在本地缓存中的key
 export default {
   name: 'search',
   data () {
     return {
-      q: '' // 关键字的数据
+      q: '', // 关键字的数据
+      historyList: JSON.parse(localStorage.getItem(key) || '["葡萄干","动漫","马云"]') // 作为一个数据变量 接收 搜索历史记录
+    }
+  },
+  methods: {
+    // 删除历史
+    delHistory (index) {
+      // 删除要先在data中删除数据 然后把data中的数据同步到 本地缓存中
+      this.historyList.splice(index, 1) // 直接删除对应的历史记录数据
+      // 将数据同步到 本地缓存
+      localStorage.setItem(key, JSON.stringify(this.historyList))
+    },
+    toResult (text) {
+      this.$router.push({ path: '/search/result', query: { q: text } })
     }
   }
 }
@@ -42,32 +61,32 @@ export default {
 <style lang='less' scoped>
 .history-box {
   padding: 0 20px;
-  .head{
+  .head {
     line-height: 36px;
     color: #999;
-    .van-icon{
+    .van-icon {
       font-size: 16px;
       float: right;
-      margin-top: 10px;;
+      margin-top: 10px;
     }
   }
-  .van-cell{
+  .van-cell {
     padding: 10px 0;
   }
-  .word_btn{
-    color:#3296fa;
+  .word_btn {
+    color: #3296fa;
   }
-  .close_btn{
-    margin-top:5px;
+  .close_btn {
+    margin-top: 5px;
     color: #999;
   }
 }
-.suggest-box{
-  /deep/ .van-cell{
+.suggest-box {
+  /deep/ .van-cell {
     padding: 10px 20px;
     color: #999;
-    p{
-      span{
+    p {
+      span {
         color: red;
       }
     }

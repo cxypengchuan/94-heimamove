4719c29234f52f918a93b790725bf3cd092d8e5f
unknown <you@example.com>
2020-03-23 22:37:45 +0800

    搜索结果页面-数据渲染

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -1,15 +1,104 @@
 <template>
   <div class='container'>
-     <van-nav-bar title="搜索结果" left-arrow  @click-left="$router.back()"></van-nav-bar>
+
+    <!-- 数据渲染和首页差不多 -->
+     <van-nav-bar fixed="" title="搜索结果" left-arrow  @click-left="$router.back()"></van-nav-bar>
+      <van-list v-model="upLoading" @load="onLoad" @finished='finished'>
+        <van-cell-group>
+          <van-cell v-for="item in articles" :key="item.art_id.toString()">
+            <div class="article_item">
+              <h3 class="van-ellipsis">{{item.title}}</h3>
+              <div class="img_box" v-if="item.cover.type === 3">
+                <van-image class="w33" fit="cover" :src="item.cover.images[0]" />
+                <van-image class="w33" fit="cover" :src="item.cover.images[1]" />
+                <van-image class="w33" fit="cover" :src="item.cover.images[2]" />
+              </div>
+              <div class="img_box">
+                <van-image class="w100" fit="cover" :src="item.cover.images[0]" />
+              </div>
+              <div class="info_box">
+                <span>{{ item.aut_name }}</span>
+                <span>{{ item.comm_count }}评论</span>
+                <span>{{ item.pubdate | relTime }}</span>
+              </div>
+            </div>
+          </van-cell>
+        </van-cell-group>
+      </van-list>
   </div>
 </template>
 
 <script>
+import * as Articles from '@/api/article'// 引入搜索文章接口
 export default {
-  name: 'searchResult'
+  data () {
+    return {
+      upLoading: false,
+      finished: false,
+      articles: [],
+      page: {
+        page: 1,
+        per_page: 10
+      }
+
+    }
+  },
+  methods: {
+    // 该方法会在滚动条滚动到底部的时候执行
+    async  onLoad () {
+      // 加载数据
+      const { q } = this.$route.query // 获取query参数
+      const data = await Articles.searchArticle({ ...this.page, q })
+      // 得到的结果 应该追加到artciles数据
+      this.articles.push(...data.results) // 上拉加载触发时,将数据追到队尾
+      // 关闭上拉加载的状态
+      this.upLoading = false
+      // 如何来判断 已经上拉加载 把所有的数据全都查询过来了?
+      // 接口并没有告诉我们 什么时候结束  可以根据当前 返回的数据 是否有记录
+      // 如果你返回的查询记录是 0 我此时认为 没有下一页的数据了
+      if (data.results.length) {
+        // 我认为是有下一页数据的
+        this.page.page++ // 如果有下一页 应该把页码切到下一页
+      } else {
+        // 否则是没有下一页的
+        this.finished = true // game is over  没有数据了
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container {
+  padding-top: 46px;
+  height: 100%;
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      height: 180px;
+      width: 100%;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    span {
+      padding-right: 10px;
+    }
+  }
+}
 </style>
